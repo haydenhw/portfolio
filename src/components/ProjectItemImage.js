4b07afc96b8f58509941af9a80c61da43752e34e
hayden7913 <hayden321@gmail.com>
2020-03-08 03:08:07 -0700

    Adds blur filter to app mockups

--- src/components/ProjectItemImage.js
+++ src/components/ProjectItemImage.js
@@ -6,7 +6,7 @@ export default function ProjectsItemImage({ alt, demoUrl, imgData, className }) 
     <div className={`col9 col-screenshot ${className || ""}`}>
       <a href={demoUrl}>
         <div className="project-screenshot">
-          {imgData && <Img fluid={imgData}/>}
+          {imgData && <Img fluid={imgData} placeholderStyle={{ filter: "blur(20px)" }} />}
         </div>
       </a>
     </div>

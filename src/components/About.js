633a0237bf49bca619421a10018d2e9f46f1b414
hayden7913 <hayden321@gmail.com>
2020-06-25 17:37:48 -0700

    Fix typo

--- src/components/About.js
+++ src/components/About.js
@@ -43,25 +43,8 @@ export default function About() {
         <Divider alias="about" name="about" title="about me" topBottom/>
         <div className="about-text">
           <p className="about-bio">
-            I'm a technically oriented fullstack/fronted developer with a passion for building rich and deeply interactive web applications. I particularly enjoy working with application state libraries like Redux. My ultimate career goal is to work on software that helps bring the world to 100% renewable energy. When I manage to peel myself away from my latest software projects I'm usually rock climbing or hiking in the redwoods.
+            I'm a technically oriented fullstack/frontend developer with a passion for building rich and deeply interactive web applications. I particularly enjoy working with application state libraries like Redux. My ultimate career goal is to work on software that helps bring the world to 100% renewable energy. When I manage to peel myself away from my latest software projects I'm usually rock climbing or hiking in the redwoods.
           </p>
-          {/*<div className="about-contact-wrapper">*/}
-          {/*  <span className="about-contact-message">*/}
-          {/*      Get in touch*/}
-          {/*    </span>*/}
-          {/*  <a className="about-email-link link" href="mailto:hayden321@gmail.com">hayden321@gmail.com</a>*/}
-          {/*  <div className="contact">*/}
-          {/*    <div className="contact-links">*/}
-          {/*      <a className="about-contact-link icon-github" href="https://github.com/hayden7913"/>*/}
-          {/*      <a*/}
-          {/*        className="about-contact-link icon-linkedin"*/}
-          {/*        href="https://www.linkedin.com/in/hayden-harkwright-2588b254"*/}
-          {/*      >*/}
-          {/*      </a>*/}
-          {/*      <a className="about-contact-link icon-mail" href="mailto:hayden321@gmail.com"/>*/}
-          {/*    </div>*/}
-          {/*  </div>*/}
-          {/*</div>*/}
         </div>
       </div>
     </section>

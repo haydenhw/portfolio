08f3f4563fe2ab78af0c6e73b3f0afa7ce86ef5b
hayden7913 <hayden321@gmail.com>
2020-08-16 14:36:57 -0700

    Removes unused files

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-bucket_name=haydenhw.com
-
-npm run build
-aws2 s3 sync build s3://$bucket_name 
-
-
--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.use(express.static('build'))
-// app.use('/images', express.static('public/images'))
-
-const PORT = process.env.PORT || 3002;
-
-app.listen(PORT, () => console.log('App listening on port ' + PORT + '!'));
